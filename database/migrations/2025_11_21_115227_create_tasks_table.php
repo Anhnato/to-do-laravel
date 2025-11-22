8fc62b2702fb45c59f38b5fa4497ce9ee770b967
Anhnaoto <50966044+Anhnato@users.noreply.github.com>
2025-11-22 18:21:41 +0700

    database design

---
+++ database/migrations/2025_11_21_115227_create_tasks_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')
+            ->nullable();
+            $table->enum('priority', ['high','medium','low'])
+            ->default('medium');
+            $table->enum('status',['pending','completed'])
+            ->default('pending');
+            $table->date('due_date')
+            ->nullable();
+
+            //Allow tasks without categories
+            $table->foreignId('category_id')
+            ->nullable()
+            ->constrained()
+            ->onDelete('set null');
+
+            //User who own the task
+            $table->foreignId('user_id')
+            ->constrained()
+            ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
